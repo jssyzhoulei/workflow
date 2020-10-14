7257ba1ceb2f06db9ec9f49dd2df2e36d9e87feb
xinru.ye <965691191@qq.com>
2020-10-14 19:13:10 +0800

    通过id获取用户

--- src/apis/userApi.go
+++ src/apis/userApi.go
@@ -7,10 +7,12 @@ import (
 	"gitee.com/grandeep/org-svc/utils/src/pkg/log"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 type userApiInterface interface {
 	AddUserApi(ctx *gin.Context)
+	GetUserByIDApi(ctx *gin.Context)
 }
 
 type userApi struct {
@@ -52,6 +54,25 @@ func (u *userApi) AddUserApi(ctx *gin.Context) {
 		return
 	}
 	responses(ctx, http.StatusOK, "创建用户成功", nil)
+}
+
+func (u *userApi) GetUserByIDApi(ctx *gin.Context) {
+	uid, err := strconv.Atoi(ctx.Param("uid"))
+
+	if err != nil {
+		responses(ctx, http.StatusBadRequest, "用户ID类型错误", nil)
+		return
+	}
+
+	user, err := u.userService.GetUserByIDSvc(context.Background(), uid)
+
+	if err != nil {
+		log.Logger().Error("获取用户错误：" + err.Error())
+		responses(ctx, http.StatusBadRequest, "获取用户失败", nil)
+		return
+	}
+
+	responses(ctx, http.StatusOK, "获取用户成功", user)
 }
 
 
--- src/endpoints/userServiceEndpoint.go
+++ src/endpoints/userServiceEndpoint.go
@@ -3,6 +3,7 @@ package endpoints
 import (
 	"context"
 	"errors"
+	"gitee.com/grandeep/org-svc/src/models"
 	pb_user_v1 "gitee.com/grandeep/org-svc/src/proto/user/v1"
 	"gitee.com/grandeep/org-svc/src/services"
 	"github.com/go-kit/kit/endpoint"
@@ -12,12 +13,14 @@ import (
 type UserServiceEndpoint struct {
 	// 声明 User 下的所有接口
 	AddUserEndpoint endpoint.Endpoint
+	GetUserByIDEndpoint endpoint.Endpoint
 }
 
 // NewUserEndpoint UserServiceEndpoint 的构造函数
 func NewUserEndpoint(service services.ServiceI) *UserServiceEndpoint {
 	var userServiceEndpoint = &UserServiceEndpoint{}
 	userServiceEndpoint.AddUserEndpoint = MakeAddUserEndpoint(service.GetUserService())
+	userServiceEndpoint.GetUserByIDEndpoint = MakeGetUserByIDEndpoint(service.GetUserService())
 	return userServiceEndpoint
 }
 
@@ -25,7 +28,7 @@ var (
 	RequestParamsTypeError = errors.New("request params type error")
 )
 
-// MakeAddUserEndpoint 创建添加用户端点，把服务包装成 Endpoint，传入 group 接口
+// MakeAddUserEndpoint 创建添加用户端点，把服务包装成 Endpoint，传入 user 接口
 func MakeAddUserEndpoint(userService services.UserServiceInterface) endpoint.Endpoint {
 	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
 		user, ok := request.(pb_user_v1.UserProto)
@@ -37,6 +40,18 @@ func MakeAddUserEndpoint(userService services.UserServiceInterface) endpoint.End
 	}
 }
 
+// MakeGetUserByIDEndpoint ...
+func MakeGetUserByIDEndpoint(userService services.UserServiceInterface) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		user, ok := request.(pb_user_v1.UserProto)
+		if !ok {
+			return nil, RequestParamsTypeError
+		}
+		response, err = userService.GetUserByIDSvc(ctx, &user)
+		return
+	}
+}
+
 // AddUserSvc ...
 func (u *UserServiceEndpoint) AddUserSvc(ctx context.Context, user *pb_user_v1.UserProto) (*pb_user_v1.NullResponse, error) {
 	resp, err := u.AddUserEndpoint(ctx, user)
@@ -44,4 +59,13 @@ func (u *UserServiceEndpoint) AddUserSvc(ctx context.Context, user *pb_user_v1.U
 		return nil, err
 	}
 	return resp.(*pb_user_v1.NullResponse), nil
+}
+
+// GetUserByIDSvc ...
+func (u *UserServiceEndpoint) GetUserByIDSvc(ctx context.Context, id int) (models.User, error) {
+	resp, err := u.GetUserByIDEndpoint(ctx, id)
+	if err != nil {
+		return models.User{}, err
+	}
+	return resp.(models.User), nil
 }
