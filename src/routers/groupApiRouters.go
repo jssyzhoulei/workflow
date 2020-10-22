bf2efa205697013b6514ab383b935b69eeba32d9
wangyong <17016027@qq.com>
2020-10-22 16:35:38 +0800

    组及其配额查询改为post

--- src/routers/groupApiRouters.go
+++ src/routers/groupApiRouters.go
@@ -9,7 +9,7 @@ func groupAPIRouters(g *gin.RouterGroup, api apis.IApis) {
 	g.POST("/group", api.GetGroupApis().GroupAddAPI)
 	g.PUT("/group", api.GetGroupApis().GroupUpdateAPI)
 	g.DELETE("/group", api.GetGroupApis().GroupDelete)
-	g.GET("/group/quota", api.GetGroupApis().GroupQueryWithQuotaAPI)
+	g.POST("/group/quota", api.GetGroupApis().GroupQueryWithQuotaAPI)
 	g.GET("/group/tree", api.GetGroupApis().GroupTreeQueryAPI)
 	g.GET("/group/user", api.GetGroupApis().QueryGroupAndSubGroupsUsers)
 	g.PUT("/quota", api.GetGroupApis().QuotaUpdateAPI)
